2eb9293f632611704ab4bdd93d8730649f19314a
Bắc Vương Toàn <43468251+vuongbac@users.noreply.github.com>
2020-08-25 16:52:13 +0700

    Initial Commit

---
+++ DemoCollectionViewTests/DemoCollectionViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoCollectionViewTests.swift
+//  DemoCollectionViewTests
+//
+//  Created by Vương Toàn Bắc on 8/25/20.
+//  Copyright © 2020 VTB. All rights reserved.
+//
+
+import XCTest
+@testable import DemoCollectionView
+
+class DemoCollectionViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
